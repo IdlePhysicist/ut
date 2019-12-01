7ab2aca8809ff7d962a39bb6292474044cc74c8e
idlephysicist <idlephysicist@vivaldi.net>
2019-11-30 17:03:34 -0700

    restructured the project into a lib, with a cli tool

--- Makefile
+++ Makefile
@@ -4,12 +4,13 @@ version="0.1"
 ldflags="-X main.commit=`git rev-list -1 HEAD | head -c 8` -X main.version=$(version)"
 
 BUILD=build
+CMD=cmd
 INSTALL=$(GOPATH)/bin
 
-default: build
+default: clean build
 
-build: clean
-	env CGO_ENABLED=$(cgo) GOOS=$(os) go build --ldflags $(ldflags) -o $(BUILD)/ut main.go
+build:
+	env CGO_ENABLED=$(cgo) GOOS=$(os) go build --ldflags $(ldflags) -o $(BUILD)/ut $(CMD)/main.go
 
 clean: 
 	rm -f $(BUILD)/*
@@ -19,4 +20,4 @@ install: build
 	mv $(BUILD)/ut $(INSTALL)
 
 uninstall:
-	rm $(GOPATH)/bin/ut
+	rm $(INSTALL)/ut
---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "fmt"
+  "os"
+
+	flag "github.com/spf13/pflag"
+
+	"github.com/idlephysicist/ut"
+)
+
+var (
+  version string
+  commit  string
+  versionFlg bool
+
+  format string
+  input  string
+  noon   bool
+)
+
+func main() {
+	flag.BoolVarP(&versionFlg, `version`, `v`, false, "Print version and exit")
+  flag.BoolVarP(&noon, `noon`, `n`, false, "Make returned datetime noon on that date")
+  flag.StringVarP(&format, `format`, `f`, ``, "The format to be used by the program")
+  flag.Parse()
+  input = flag.Arg(0)
+
+  if versionFlg {
+    fmt.Printf("ut version: %s\nbuild commit: %s\n", version, commit)
+    os.Exit(0)
+  }
+
+	result, err := ut.Ut(input, format, noon)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	} else {
+		fmt.Println(result)
+	}
+}
